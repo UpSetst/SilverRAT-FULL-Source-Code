a68a2ab9b327d97294e0fe8151e67a3d6367107e
Jose6453289 <hjghfdsds@outlook.com>
2024-10-07 17:10:31 +0400

    AssemblyInfo.cs

---
+++ SRC/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SilverRAT")]
+[assembly: AssemblyConfiguration("cracked")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("SilverRAT")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("df1779d1-a1c3-4f48-8bff-3360b072d16f")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyVersion("1.0.0.0")]
